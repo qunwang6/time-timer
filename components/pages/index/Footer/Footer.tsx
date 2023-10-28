614b5560a5c14dd5024c87e85a539e71a5030753
qunwang6 <qunwang6@gmail.com>
2023-10-28 17:07:03 +0800

    dd

--- components/pages/index/Footer/Footer.tsx
+++ components/pages/index/Footer/Footer.tsx
@@ -65,7 +65,7 @@ export default function Footer() {
               onClick={onTimerStartClick}
             />
           ) : (
- 
+            <span></span>
           )
         ) : null}
       </Container>
