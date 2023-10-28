08c26b1d0bb3ab95636dacb9a927391cc3e42926
qunwang6 <qunwang6@gmail.com>
2023-10-28 15:37:56 +0800

    vvv

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -2,11 +2,17 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+  // apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+  // projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+
+  apiKey: "AIzaSyDA1FAe9_b6ts2uPDMxUtlxHCuWGmH75L0",
+  projectId: "timer-ce84d",
+  storageBucket: "timer-ce84d.appspot.com",
+  messagingSenderId: "309616404237",
+  appId: "1:309616404237:web:6888844716e7c6e294b244",
 };
 
 if (!firebase.apps.length) {
