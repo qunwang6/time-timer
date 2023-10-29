1629fbac52bb700596a542561f3947ef86982126
qunwang6 <qunwang6@gmail.com>
2023-10-29 12:50:54 +0800

    aa

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -35,7 +35,8 @@ export default function useAudio(
       audio.onloadeddata = () => {
         //
         audio.autoplay = true;
-        
+        alert("audio");
+
         audio.play();
       };
 
